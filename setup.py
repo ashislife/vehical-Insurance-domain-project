3027990351fa71e19df38c56097e755d2a240c90
Ashish Mehra <ashismehra095@gmail.com>
2026-02-16 22:13:49 +0530

    project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ashish",
+    author_email="ashismehra095@gmail.com",
+    packages=find_packages()
+)
